088e65ab0cda28b9df864e8494abedee6c150580
William Akoto <akotowilliam92@gmail.com>
2025-05-23 15:48:06 +0000

    Add basic arithmetic file

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,17 @@
+#Assigning values to variables
+
+number1 = 10
+number2 = 5
+
+#Performing Basic Arithmetic Operations
+
+Addition = number1 + number2
+Substraction = number1 - number2
+Multiplication = number1 * number2
+
+#Printing the Results
+print("The Addition of", number1, "and", number2, "is", Addition)
+print("The Substraction of", number1, "and", number2, "is", Substraction)
+print("The Multiplication of", number1, "and", number2, "is", Multiplication)
+
+
