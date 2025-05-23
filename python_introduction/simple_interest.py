56e29c2275ec2b707efd2b24b73475de0357ac9f
William Akoto <akotowilliam92@gmail.com>
2025-05-23 16:24:13 +0000

    Add simple_interest file to calculate interest

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,13 @@
+#Defining the variables and assigning values
+
+principal = 1000
+rate = 0.05
+time = 3
+
+#Calculating the Interest
+
+interest = principal * rate * time
+
+#Displaying the Results of the Calculated Interest
+
+print("The simple interest is :",interest) 
