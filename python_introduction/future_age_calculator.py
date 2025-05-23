26bb955bb572b3167cd191c72b4853f0b8503473
William Akoto <akotowilliam92@gmail.com>
2025-05-23 17:39:49 +0000

    Add future age calculator file

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,11 @@
+#Define Variable to ask for the current Age
+
+Age = int(input("How old are you? "))
+
+#Calculate Age in 2050
+
+Age_In_2050 = Age + 27
+
+#Display Age of User in 2050
+
+print("In 2050, you will be",Age_In_2050,"years old")
