a22fd7d9e8fc6a2ce9f6aaebc509eed73952ecdc
William Akoto <akotowilliam92@gmail.com>
2025-06-08 14:49:22 +0000

    Add explore date file

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,18 @@
+import datetime
+
+#PART ONE
+
+def display_current_datetime():
+	current_date = datetime.datetime.now()
+	print(f"Current date and time:{current_date.strftime('%Y-%m-%d %H:%M:%S')}")#Print Specific Time Format
+	return current_date
+
+#PART TWO
+
+def calculate_future_date():
+	current_date = display_current_datetime()
+	number_of_days = int(input("Enter the number of days to add to the cuttent date: "))
+	future_date = current_date + datetime.timedelta(days=number_of_days)
+	print(f"Future date:{future_date.strftime('%Y-%m-%d')}")
+
+calculate_future_date()  
