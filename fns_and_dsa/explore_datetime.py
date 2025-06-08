9409747c0c900c036aeeb2f87de5ca7861052020
William Akoto <akotowilliam92@gmail.com>
2025-06-08 16:18:32 +0000

    Corrected Syntax and Spelling Errors

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,9 +1,9 @@
-import datetime
+from datetime import datetime, timedelta
 
 #PART ONE
 
 def display_current_datetime():
-	current_date = datetime.datetime.now()
+	current_date = datetime.now()
 	print(f"Current date and time:{current_date.strftime('%Y-%m-%d %H:%M:%S')}")#Print Specific Time Format
 	return current_date
 
@@ -11,8 +11,8 @@ def display_current_datetime():
 
 def calculate_future_date():
 	current_date = display_current_datetime()
-	number_of_days = int(input("Enter the number of days to add to the cuttent date: "))
-	future_date = current_date + datetime.timedelta(days=number_of_days)
+	number_of_days = int(input("Enter the number of days to add to the current date: "))
+	future_date = current_date + timedelta(days=number_of_days)
 	print(f"Future date:{future_date.strftime('%Y-%m-%d')}")
 
 calculate_future_date()  
